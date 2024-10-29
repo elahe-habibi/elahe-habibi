694b87713be60396870b69e4da7537b7fec1a3af
e.habibi <e.habibi@fanap.ir>
2024-10-29 12:14:12 +0330

    add-chane

---
+++ test.js
@@ -0,0 +1,17 @@
+
+
+To have this happen automatically for branches without a tracking
+upstream, see 'push.autoSetupRemote' in 'git help config'.
+
+PS C:\Users\Fanap_Dotin\Desktop\my-project> git push -u my-test cypress
+info: please complete authentication in your browser...
+Enumerating objects: 6, done.
+Counting objects: 100% (6/6), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (6/6), 483 bytes | 483.00 KiB/s, done.
+Total 6 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/elahe-habibi/elahe-habibi.git
+ * [new branch]      cypress -> cypress
+branch 'cypress' set up to track 'my-test/cypress'.
+PS C:\Users\Fanap_Dotin\Desktop\my-project>   
